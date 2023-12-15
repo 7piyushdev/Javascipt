ee13971677f2ac2e8fac5a0b215fd3ddb6856a64
7piyushdev <dchlamydia@gmail.com>
2023-12-16 00:05:19 +0530

    datatypes and test edited

---
+++ .devcontainer/datatypes.js
@@ -0,0 +1,10 @@
+"use strict"; // this wil treat the code as newer versions of JavaScript through JS engine.
+
+// alert("bawa");  alert is not defined because we're using node.js environment here and alert is used by js and can be run in a browser.
+
+// https://tc39.es/           ECMASCRIPT standards used for JS.
+
+// DataType
+
+// null => standalone value OR representation of empty vallue.  EX--  let state = null;
+//symbol => used for uniqueness.
--- .devcontainer/test.js
+++ .devcontainer/test.js
@@ -1,1 +1,11 @@
-console.log("Piyush verma is practicing javascript");
+const account_id = 2;
+let email = "abc@gmail.com";
+var password = "12345";
+accCity = "Kalka";
+let accPlace;
+
+email = "xyz@yahoo.com";
+password = "09876";
+accCity = "Panchkula";
+
+console.table([account_id, email, password, accCity, accPlace]);
